6f0fa041849c504619c1846de695605a9ddef928
Nicklas185105 <s185105@student.dtu.dk>
2019-04-02 06:34:36 +0200

    Initial commit

---
+++ src/mandatory41.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class mandatory41 {
+
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        String N = "N";
+        String R = "R";
+
+        String[] A = new String[n];
+        int[] B = new int[n];
+        int[] C = new int[n];
+
+        
+
+
+    }
+
+}
